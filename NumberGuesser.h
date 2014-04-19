f401d924862b59d62f83b7ef36d23cb8f97257b4
svn <svn@d9013ba4-f9c6-e311-acb0-001cc0fd6b5d>
2014-04-19 00:24:15 +0000

    Added license and comments to header file
    git-svn-id: svn+ssh://svn.pricetx.org/home/svn/numberguesser@34 d9013ba4-f9c6-e311-acb0-001cc0fd6b5d

--- NumberGuesser.h
+++ NumberGuesser.h
@@ -1,51 +1,90 @@
+/*
+* Copyright (c) 2014, Jonathan Price
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice, this
+* list of conditions and the following disclaimer in the documentation and/or
+* other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* Time limit for the time gamemode */
 #ifndef TIMELIMIT
 #define TIMELIMIT 25
 #endif
 
+/* Return value to represent easy difficulty */
 #ifndef DIFF_EASY
 #define DIFF_EASY 10
 #endif
 
+/* Return value to represent medium difficulty */
 #ifndef DIFF_MEDIUM
 #define DIFF_MEDIUM 20
 #endif
 
+/* Return value to represent hard difficulty */
 #ifndef DIFF_HARD
 #define DIFF_HARD 30
 #endif
 
+/* Return value to represent attempts gamemode */
 #ifndef MODE_ATTEMPTS
 #define MODE_ATTEMPTS 10
 #endif
 
+/* Return value to represent time gamemode */
 #ifndef MODE_TIME
 #define MODE_TIME 20
 #endif
 
+/* Return value to represent help mode */
 #ifndef MODE_HELP
 #define MODE_HELP 30
 #endif
 
+/* Maximum number for easy difficulty */
 #ifndef EASY_MAX
 #define EASY_MAX 100
 #endif
 
+/* Maximum number for medium difficulty */
 #ifndef MEDIUM_MAX
 #define MEDIUM_MAX 1000
 #endif
 
+/* Maximum number for hard difficulty */
 #ifndef HARD_MAX
 #define HARD_MAX 5000
 #endif
 
+/* Maximum number of attempts for easy difficulty */
 #ifndef EASY_ATTEMPTS
 #define EASY_ATTEMPTS 10
 #endif
 
+/* Maximum number of attempts for medium difficulty */
 #ifndef MEDIUM_ATTEMPTS
 #define MEDIUM_ATTEMPTS 15
 #endif
 
+/* Maximum number of attempts for hard difficulty */
 #ifndef HARD_ATTEMPTS
 #define HARD_ATTEMPTS 25
 #endif
